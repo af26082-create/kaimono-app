154048fc0b6e4438265dd0165bd544266d2b827f
af26082-create <af26082@shibaura-it.ac.jp>
2026-09-08 17:23:00 +0900

    feat: アプリ名をお買い物メモに変更 (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,11 @@ function App() {
 
   return (
     <div className="app">
-      <h1>買い物・支出マネージャー</h1>
+
+      <h1>お買い物メモ</h1>
+
+
+
 
       {/* ItemList に4つの props を渡す。
           items        … 表示したい項目の配列
